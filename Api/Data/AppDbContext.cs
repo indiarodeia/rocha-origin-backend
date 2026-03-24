b5b03b65149f5ecca93efa1f7c966a5becaffa9c
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2026-03-23 23:54:26 -0100

    added data connection

---
+++ Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+}
