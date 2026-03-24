af271910f6954ba2216a22b65a588a601641b45c
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2026-03-24 00:01:14 -0100

    added address model

---
+++ Api/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models
+{
+    [Table("addresses")]
+    public class Address
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(255)]
+        public string Street { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string DoorNumber { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Country { get; set; } = string.Empty;
+    }
+}
